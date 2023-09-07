053ee6bde20f9a1c57ed6cb0cae12d11042b3270
Sandor Hajdu <125272342+hsaz213@users.noreply.github.com>
2023-09-08 01:10:19 +0200

    refactor, modularize guest grouping, remove magic numbers

--- src/main/java/com/codecool/ehotel/service/BreakfastGroups.java
+++ src/main/java/com/codecool/ehotel/service/BreakfastGroups.java
@@ -7,39 +7,48 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
-import static java.lang.Math.floor;
-
 public class BreakfastGroups {
+    private static final int NUMBER_OF_GROUPS = 8;
 
     public ArrayList<List<Guest>> createGroups(Set<Guest> guests) {
         List<Guest> guestList = new ArrayList<>(guests);
-
         int guestCount = guests.size();
 
-        ArrayList<Integer> groupSplitIndexArr = new ArrayList<>(8);
-        int firstIndex = 0;
-        groupSplitIndexArr.add(firstIndex);
-        groupSplitIndexArr.add(guestCount);
-        for (int i = 0; i < 7; i++) {
-            int splitIndex = (int) floor(Math.random() * guestCount);
-            groupSplitIndexArr.add(splitIndex);
-        }
-        groupSplitIndexArr.sort(Comparator.comparing(Integer::valueOf));
+        ArrayList<Integer> groupSplitIndexArr = createGroupSplitIndices(guestCount);
 
         System.out.println(groupSplitIndexArr);
 
-        int numberOfCycles = 8;
+        ArrayList<List<Guest>> listOfGroups = createListOfGroups(guestList, groupSplitIndexArr);
 
-        ArrayList<List<Guest>> listOfGroups = new ArrayList<List<Guest>>(8);
-        for (int i = 0; i < numberOfCycles; i++) {
+        logGroups(listOfGroups);
+        return listOfGroups;
+    }
+
+    private ArrayList<Integer> createGroupSplitIndices(int guestCount) {
+        ArrayList<Integer> groupSplitIndexArr = new ArrayList<>(NUMBER_OF_GROUPS);
+        groupSplitIndexArr.add(0);
+        groupSplitIndexArr.add(guestCount);
+        for (int i = 0; i < NUMBER_OF_GROUPS - 1; i++) {
+            int splitIndex = (int) Math.floor(Math.random() * guestCount);
+            groupSplitIndexArr.add(splitIndex);
+        }
+        groupSplitIndexArr.sort(Comparator.comparing(Integer::valueOf));
+        return groupSplitIndexArr;
+    }
+
+    private ArrayList<List<Guest>> createListOfGroups(List<Guest> guestList, ArrayList<Integer> groupSplitIndexArr) {
+        ArrayList<List<Guest>> listOfGroups = new ArrayList<>(NUMBER_OF_GROUPS);
+        for (int i = 0; i < NUMBER_OF_GROUPS; i++) {
             int subStartIndex = groupSplitIndexArr.get(i);
             int subEndIndex = groupSplitIndexArr.get(i + 1);
-            listOfGroups.add(guestList.subList(subStartIndex, subEndIndex));
-        }
-
-        for (int i = 0; i < 8; i++) {
-            System.out.println("Group " + String.valueOf(i + 1) + " " + listOfGroups.get(i));
+            listOfGroups.add(new ArrayList<>(guestList.subList(subStartIndex, subEndIndex)));
         }
         return listOfGroups;
+    }
+
+    private void logGroups(ArrayList<List<Guest>> listOfGroups) {
+        for (int i = 0; i < NUMBER_OF_GROUPS; i++) {
+            System.out.println("Group " + (i + 1) + ": " + listOfGroups.get(i));
+        }
     }
 }
